78ffdb4d63c388a2825c9f00fe8d235b15b00092
karlpn <91100704+karlpn@users.noreply.github.com>
2024-10-30 03:49:29 +0100

    Footer components added

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,99 @@
+import MaxWidthWrapper from "../../utils/MaxWidthWrapper";
+import { SiInstagram, SiX, SiYoutube } from "react-icons/si";
+import GenericColumn from "./GenericColumn";
+import LogoColumn from "./LogoColumn";
+
+const Footer = () => {
+  return (
+    <footer className="relative overflow-hidden py-12 bg-defaultBg font-inter border-t border-borderClr">
+      <MaxWidthWrapper className="relative z-20 grid grid-cols-12 gap-x-1.5 gap-y-6">
+        <LogoColumn />
+        <GenericColumn
+          title="Explore Holidaze"
+          links={[
+            {
+              title: "Home",
+              href: "/",
+            },
+            {
+              title: "Browse Venues",
+              href: "/",
+            },
+            {
+              title: "Become a Host",
+              href: "/",
+            },
+            {
+              title: "How it works",
+              href: "/",
+            },
+            {
+              title: "Contact us",
+              href: "/",
+            },
+          ]}
+        />
+        <GenericColumn
+          title="Company Info"
+          links={[
+            {
+              title: "About us",
+              href: "/",
+            },
+            {
+              title: "Careers",
+              href: "/",
+            },
+            {
+              title: "Press",
+              href: "/",
+            },
+            {
+              title: "Privacy Policy",
+              href: "/",
+            },
+            {
+              title: "Terms & Conditions",
+              href: "/",
+            },
+          ]}
+        />
+        <GenericColumn
+          title="Socials"
+          links={[
+            {
+              title: "Twitter",
+              href: "/#",
+              Icon: SiX,
+            },
+            {
+              title: "Instagram",
+              href: "/#",
+              Icon: SiInstagram,
+            },
+            {
+              title: "Youtube",
+              href: "/#",
+              Icon: SiYoutube,
+            },
+          ]}
+        />
+        <GenericColumn
+          title="Contact us"
+          links={[
+            {
+              title: "Email: support@holidaze.com",
+              href: "/#",
+            },
+            {
+              title: "Phone: +1 234 567 890",
+              href: "/#",
+            },
+          ]}
+        />
+      </MaxWidthWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
