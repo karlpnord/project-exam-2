ca1b3449383f6bceaf3064822c400642088d34e8
karlpn <91100704+karlpn@users.noreply.github.com>
2024-10-31 03:44:56 +0100

    Axios installed. usePosts hook implemented to carousel cards on homepage.

--- src/components/home/popular-venues/Carousel.tsx
+++ src/components/home/popular-venues/Carousel.tsx
@@ -1,13 +1,16 @@
 import useMeasure from "react-use-measure";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 import Card from "./Card";
+import { usePosts } from "../../../hooks/usePosts";
+import { VenueData } from "../../../types/venueTypes";
+import Loader from "../../../utils/Loader";
+import PrevNextButtons from "./PrevNextButtons";
 
-const CARDS_AMOUNT = 5;
+const CARDS_AMOUNT = 10;
 
 const CARD_WIDTH = 320;
-const MARGIN = 20;
+const MARGIN = 30;
 const CARD_SIZE = CARD_WIDTH + MARGIN;
 
 const BREAKPOINTS = {
@@ -19,6 +22,14 @@ const Carousel = () => {
   const [ref, { width }] = useMeasure();
   const [offset, setOffset] = useState(0);
 
+  const { data, isLoading, isSuccess } = usePosts();
+
+  let filteredVenues: VenueData[] = [];
+  
+  if (isSuccess) {
+    filteredVenues = [...data.data].slice(0, CARDS_AMOUNT);
+  }
+  
   const CARD_BUFFER =
     width > BREAKPOINTS.lg ? 4 : width > BREAKPOINTS.sm ? 2 : 1;
 
@@ -54,81 +65,36 @@ const Carousel = () => {
             </div>
           </div>
 
-          <motion.div
-            animate={{
-              x: offset,
-            }}
-            transition={{
-              ease: "easeInOut",
-            }}
-            className="grid"
-            style={{
-              gridTemplateColumns: `repeat(${CARDS_AMOUNT}, 1fr)`,
-            }}
-          >
-            <Card
-              className="shrink-0"
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <motion.div
+              animate={{
+                x: offset,
+              }}
+              transition={{
+                ease: "easeInOut",
+              }}
+              className="grid"
               style={{
-                width: CARD_WIDTH,
-                marginRight: MARGIN,
+                gridTemplateColumns: `repeat(${CARDS_AMOUNT}, 1fr)`,
               }}
             >
-            </Card>
-            <Card
-              className="shrink-0"
-              style={{
-                width: CARD_WIDTH,
-                marginRight: MARGIN,
-              }}
-            >
-            </Card>
-            <Card
-              className="shrink-0"
-              style={{
-                width: CARD_WIDTH,
-                marginRight: MARGIN,
-              }}
-            >
-            </Card>
-            <Card
-              className="shrink-0"
-              style={{
-                width: CARD_WIDTH,
-                marginRight: MARGIN,
-              }}
-            >
-            </Card>
-            <Card
-              className="shrink-0"
-              style={{
-                width: CARD_WIDTH,
-                marginRight: MARGIN,
-              }}
-            >
-            </Card>
-          </motion.div>
-          
-          <div className="flex items-center justify-center gap-2 mt-10">
-            <button
-              className={`rounded-md border-[1px] border-borderClr bg-foreground p-2.5 text-2xl transition-opacity ${
-                CAN_SHIFT_LEFT ? "" : "opacity-30"
-              }`}
-              disabled={!CAN_SHIFT_LEFT}
-              onClick={shiftLeft}
-            >
-              <FiArrowLeft />
-            </button>
-            <button
-              className={`rounded-md border-[1px] border-borderClr bg-foreground p-2.5 text-2xl transition-opacity ${
-                CAN_SHIFT_RIGHT ? "" : "opacity-30"
-              }`}
-              disabled={!CAN_SHIFT_RIGHT}
-              onClick={shiftRight}
-            >
-              <FiArrowRight />
-            </button>
-          </div>
-
+              {filteredVenues.map((venue) => (
+                <Card
+                  key={venue.id}
+                  data={venue}
+                  className="shrink-0"
+                  style={{
+                    width: CARD_WIDTH,
+                    marginRight: MARGIN,
+                  }}
+                >
+                </Card>
+              ))}
+            </motion.div>
+          )} 
+          <PrevNextButtons shiftRight={CAN_SHIFT_RIGHT} shiftLeft={CAN_SHIFT_LEFT} onClickPrev={shiftLeft} onClickNext={shiftRight}/>
         </div>
       </div>
     </section>
