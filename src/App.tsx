7f461d805630c8ff8c663659d7db75df51b2313f
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-01 03:38:42 +0100

    Routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Home from "./pages/home/Home";
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/home/Home';
+import AllVenues from './pages/all-venues/AllVenues';
 
 const App = () => {
-  return (
-    <Home />
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path='/all-venues' element={<AllVenues />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
