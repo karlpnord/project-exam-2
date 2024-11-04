f9b6f9518d1986eac6a149dfa0e249c9c61dae7e
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-04 21:22:07 +0100

    Register Page Component updated

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,12 +1,14 @@
 import MaxWidthWrapper from '../../utils/MaxWidthWrapper';
 import image from '../../assets/image-2.png';
 import RegisterForm from '../../components/forms/RegisterForm';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Register = () => {
   return (
     <main className="flex-1 bg-whiteBg font-inter">
       <MaxWidthWrapper className="relative z-20 flex flex-col items-center justify-center pb-12 pt-24 md:pb-36 md:pt-36">
-        <div className="bg-foreground rounded-md shadow-md overflow-hidden px-6 py-8 w-full max-w-[540px] lg:max-w-7xl lg:flex lg:p-0">
+        <div className="bg-foreground rounded-md shadow-md overflow-hidden px-6 py-8 w-full max-w-[540px] lg:min-h-[700px] lg:max-w-7xl lg:flex lg:p-0">
           <figure className="hidden lg:flex lg:w-1/2 flex-shrink-0">
             <div className="relative w-full aspect-ratio aspect-ratio-16/9">
               <img
@@ -15,18 +17,28 @@ const Register = () => {
               />
             </div>
           </figure>
-          <div className="flex flex-col gap-6 w-full lg:w-1/2 p-8 xl:p-12">
+          <motion.div
+            initial={{ y: -250, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              ease: 'easeOut',
+              duration: 1,
+            }}
+            className="flex flex-col self-center gap-6 w-full lg:w-1/2 lg:px-12"
+          >
             <div>
               <h1 className="text-4xl text-textDark font-semibold mb-1">
                 Create an account
               </h1>
               <h2 className="text-textLight text-sm">
                 Already have an account?{' '}
-                <span className="underline text-secondary">Log in</span>
+                <Link to="/sign-in" className="underline text-secondary">
+                  Log in
+                </Link>
               </h2>
             </div>
             <RegisterForm />
-          </div>
+          </motion.div>
         </div>
       </MaxWidthWrapper>
     </main>
