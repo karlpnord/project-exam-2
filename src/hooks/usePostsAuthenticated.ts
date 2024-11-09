432072abe463856dbca64fce3700cbc565f7bd97
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-10 00:13:02 +0100

    Custom hook for fetching venues with authentication

---
+++ src/hooks/usePostsAuthenticated.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const usePostsAuthenticated = (
+  url: string,
+  token: string | undefined
+) => {
+  return useQuery({
+    queryKey: ['my-venues'],
+    queryFn: async () => {
+      if (!token) {
+        throw new Error('User not authenticated');
+      }
+      const { data } = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'X-Noroff-API-Key': API_KEY,
+        },
+      });
+      return data;
+    },
+  });
+};
