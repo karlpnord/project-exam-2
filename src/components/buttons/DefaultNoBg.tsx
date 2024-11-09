bf81dac769385251cb289eccf912941d487abc98
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-10 00:19:18 +0100

    DefaultNoBg button component updatea

--- src/components/buttons/DefaultNoBg.tsx
+++ src/components/buttons/DefaultNoBg.tsx
@@ -1,12 +1,21 @@
 import { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  className?: string;
+  clickHandler?: () => void;
 }
 
-const DefaultNoBg = ({ children }: Props) => {
+const DefaultNoBg = ({ children, className = '', clickHandler }: Props) => {
   return (
-    <button className="bg-whiteBg text-textDark px-4 py-2 rounded-md text-lg font-medium font-inter hover:bg-defaultBg transition-all">
+    <button
+      className={twMerge(
+        'bg-whiteBg text-textDark px-4 py-2 rounded-md text-lg font-medium font-inter hover:bg-defaultBg transition-all',
+        className
+      )}
+      onClick={clickHandler}
+    >
       {children}
     </button>
   );
