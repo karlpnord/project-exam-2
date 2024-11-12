bc8b905beab756032e4a3e23c958cab6a6164f9c
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-13 00:55:09 +0100

    Slight adjustments to SectionHeading

--- src/utils/SectionHeading.tsx
+++ src/utils/SectionHeading.tsx
@@ -5,8 +5,10 @@ interface SectionHeadingProps {
 
 const SectionHeading = ({ heading, subheading }: SectionHeadingProps) => {
   return (
-    <div className="mt-3 mb-6 flex flex-col items-center text-center">
-      <h2 className="text-5xl mb-2 font-bold text-textDark">{heading}</h2>
+    <div className="mb-6 flex flex-col items-center text-center">
+      <h2 className="text-4xl md:text-5xl mb-2 font-bold text-textDark">
+        {heading}
+      </h2>
       <h3 className="text-sm text-textLight max-w-[700px]">{subheading}</h3>
     </div>
   );
