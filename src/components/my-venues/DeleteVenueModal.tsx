b8faf9261a99615293cd285c88665fa3eeb83c64
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-12 01:40:23 +0100

    DeleteVenueModal updated

--- src/components/my-venues/DeleteVenueModal.tsx
+++ src/components/my-venues/DeleteVenueModal.tsx
@@ -23,7 +23,7 @@ const DeleteVenueModal = ({ setIsModalOpen, confirmDelete }: Props) => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center cursor-pointer font-inter"
+          className="fixed inset-0 z-90 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center cursor-pointer font-inter"
           onClick={handleClose}
         >
           <motion.div
@@ -32,7 +32,7 @@ const DeleteVenueModal = ({ setIsModalOpen, confirmDelete }: Props) => {
             exit={{ scale: 0.9, opacity: 0 }}
             transition={{ duration: 0.3 }}
             onClick={(e) => e.stopPropagation()}
-            className="bg-gradient-to-br from-error to-red-800 p-8 rounded-md max-w-[440px] cursor-default"
+            className="bg-gradient-to-br from-error to-red-800 p-8 rounded-md max-w-[440px] cursor-default relative z-100"
           >
             <FiAlertCircle size={32} className="text-errorContent mb-4" />
             <h2 className="text-2xl font-semibold text-errorContent">
