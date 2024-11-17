9a38399448d4a2b62e04103db583b7d8e532b64c
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-17 20:41:08 +0100

    Update to booking types

--- src/types/bookingType.ts
+++ src/types/bookingType.ts
@@ -13,3 +13,35 @@ export interface BookingData {
   guests: number;
   venueId: string;
 }
+
+export interface UserBookingsData {
+  dateFrom: string;
+  dateTo: string;
+  guests: number;
+  id: string;
+  venue: {
+    created: string;
+    description: string;
+    id: string;
+    location: {
+      address: string;
+      city: string;
+      country: string;
+    };
+    media: [
+      {
+        url: string;
+        alt: string;
+      },
+    ];
+    price: number;
+    name: string;
+    maxGuests: number;
+    meta: {
+      wifi: boolean;
+      parking: boolean;
+      breakfast: boolean;
+      pets: true;
+    };
+  };
+}
