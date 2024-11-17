e8b9e9fc093bda59aa408d1b6030feb9511453b6
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-17 20:40:49 +0100

    Profile page component

---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,61 @@
+import MaxWidthWrapper from '../../utils/MaxWidthWrapper';
+import { useAuth } from '../../hooks/useAuth';
+import MyVenuesErrorContent from '../../components/my-venues/MyVenuesErrorContent';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import Tabs from '../../components/profile/Tabs';
+import ProfileTabContent from '../../components/profile/ProfileTabContent';
+import BookingsTabContent from '../../components/profile/BookingsTabContent';
+import SettingsTabContent from '../../components/profile/SettingsTabContent';
+import Logo from '../../components/header/Logo';
+
+type ActiveTab = 'profile' | 'bookings' | 'settings';
+
+const Profile = () => {
+  const [activeTab, setActiveTab] = useState<ActiveTab>('profile');
+  const { user } = useAuth();
+
+  const clickHandler = (tab: ActiveTab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <main className="flex-1 font-inter">
+      <MaxWidthWrapper className="flex flex-col gap-8 px-0 sm:px-6 pb-12 pt-24 md:pb-36 md:pt-36 max-w-[1000px]">
+        {!user && <MyVenuesErrorContent user={user} />}
+        {user && (
+          <div className="flex flex-col border border-borderClr rounded-md overflow-hidden">
+            <div className="flex flex-col w-full bg-foreground text-textDark p-4 border-b border-borderClr">
+              <Logo />
+              <h2 className="font-bold text-lg text-textLight">
+                {user.name}'s profile
+              </h2>
+            </div>
+            <div className="flex">
+              <Tabs activeTab={activeTab} clickHandler={clickHandler} />
+              <motion.div
+                key={activeTab}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="col-span-full flex-grow bg-foreground"
+              >
+                {activeTab === 'profile' && (
+                  <ProfileTabContent
+                    user={user}
+                    openSettings={() => clickHandler('settings')}
+                    openBookings={() => clickHandler('bookings')}
+                  />
+                )}
+                {activeTab === 'bookings' && <BookingsTabContent user={user} />}
+                {activeTab === 'settings' && <SettingsTabContent user={user} />}
+              </motion.div>
+            </div>
+          </div>
+        )}
+      </MaxWidthWrapper>
+    </main>
+  );
+};
+
+export default Profile;
