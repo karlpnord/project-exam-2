864bff04d4b5c7b8c0b0abcfc7422b1268b4b074
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-18 22:14:51 +0100

    Homepage FeatureCards grid update and animation update

--- src/components/home/features/FeatureCard.tsx
+++ src/components/home/features/FeatureCard.tsx
@@ -7,22 +7,21 @@ interface FeatureCardProps {
 }
 
 const FeatureCard = ({ feature }: FeatureCardProps) => {
+  const childVariants = {
+    hidden: { opacity: 0, y: 20 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.75, ease: 'easeInOut' },
+    },
+  };
+
   return (
     <motion.div
-      className={twMerge('p-[2px] rounded-md', feature.gradient)}
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 0.75,
-        delay: 0.25,
-        ease: 'easeInOut',
-      }}
+      className={twMerge('p-[2px] rounded-md flex-1', feature.gradient)}
+      variants={childVariants}
     >
-      <div className="flex flex-col gap-2 h-full w-full bg-foreground rounded-[calc(0.375rem-1px)] p-4">
+      <div className="flex flex-col gap-2 h-full w-full max-w-[360px] bg-foreground rounded-[calc(0.375rem-1px)] p-4">
         <div
           className={twMerge(
             'p-2 w-fit text-lg rounded-md',
@@ -35,7 +34,7 @@ const FeatureCard = ({ feature }: FeatureCardProps) => {
         <h2 className="font-poppins font-semibold text-xl text-textDark">
           {feature.title}
         </h2>
-        <p className="text-textLight font-light text-xs sm:test-sm">
+        <p className="text-textLight font-light text-xs leading-5 sm:test-sm">
           {feature.description}
         </p>
       </div>
--- src/components/home/features/Features.tsx
+++ src/components/home/features/Features.tsx
@@ -2,8 +2,19 @@ import MaxWidthWrapper from '../../../utils/MaxWidthWrapper';
 import FeatureCard from './FeatureCard';
 import featuresData from './featuresData';
 import SectionHeading from '../../../utils/SectionHeading';
+import { motion } from 'framer-motion';
 
 const Features = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
   return (
     <div className="bg-whiteBg font-inter">
       <MaxWidthWrapper className="py-20 flex flex-col gap-16">
@@ -13,11 +24,16 @@ const Features = () => {
             'Holidaze offers a fast, secure, and easy way to find or list venues. Explore the key features that make your booking experience seamless and stress-free.'
           }
         />
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+        <motion.div
+          className="grid mx-auto w-fit grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+          variants={containerVariants}
+          initial="hidden"
+          whileInView="show"
+        >
           {featuresData.map((feature, index) => (
             <FeatureCard key={index} feature={feature} />
           ))}
-        </div>
+        </motion.div>
       </MaxWidthWrapper>
     </div>
   );
