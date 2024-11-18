1c8caa955ae34d96b6ad37303f3f8f7bd225ac31
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-18 22:16:44 +0100

    Homepage Logos styling update

--- src/components/home/logos/Logos.tsx
+++ src/components/home/logos/Logos.tsx
@@ -3,8 +3,8 @@ import LogoItems from './LogoItems';
 
 const Logos = () => {
   return (
-    <div className="bg-whiteBg">
-      <section className="mx-auto max-w-7xl overflow-hidden border-b border-borderClr py-6">
+    <div className="bg-whiteBg border-b border-borderClr">
+      <section className="mx-auto max-w-7xl overflow-hidden py-6">
         <span className="mx-auto mb-9 block w-fit text-textLighter text-center text-lg font-poppins">
           Trusted by companies of all sizes
         </span>
