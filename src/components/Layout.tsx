a35c29143cda5779435931178f314cf68063f533
karlpn <91100704+karlpn@users.noreply.github.com>
2024-11-18 22:23:20 +0100

    Added a ScrollToTop component to always navigate to the top of a new page.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import Header from './header/Header';
 import Footer from './footer/Footer';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from '../utils/ScrollToTop';
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-whiteBg">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
